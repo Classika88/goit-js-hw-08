43304cf735056e6d7d4d543798e865ae75253254
Iryna Khamidova <waterholein@gmail.com>
2022-12-30 09:59:38 +0200

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const divEl = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+   const galleryObj = arr.map(({preview, original, description})=> {
+     
+   return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`}).join("")
+    divEl.innerHTML = galleryObj;
+}
+ createMarkup(galleryItems);
+
+ new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+     
