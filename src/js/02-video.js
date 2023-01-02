e59cd4cf6bf666066f903107e77dd2f65969ed9e
Iryna Khamidova <waterholein@gmail.com>
2023-01-02 15:04:34 +0200

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from "@vimeo/player"; 
+import { throttle } from 'lodash'; 
 
+const vimeoPlayer = document.querySelector("#vimeo-player");
+const player = new Player(vimeoPlayer);
+const currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+
+const onPlaying = function (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+    };
+player.on("timeupdate", throttle(onPlaying, 1000));
+player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import { throttle } from 'lodash'; 
+const feedbackFormEl = document.querySelector('.feedback-form');
+const userInput = {};
 
+const fillFeedbackFormFields = () => {
+  try {
+    const userInfoFromLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+
+    if (userInfoFromLS === null) {
+      return;
+    }
+
+    for (const prop in userInfoFromLS) {
+      feedbackFormEl.elements[prop].value = userInfoFromLS[prop];
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+fillFeedbackFormFields();
+
+const onFeedbackFormFieldChange = event => {
+    const { target } = event;
+
+    const fieldValue = target.value;
+    const fieldName = target.name;
+
+    userInput[fieldName] = fieldValue;
+
+    try {
+        localStorage.setItem('feedback-form-state', JSON.stringify(userInput));
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const onFeedbackFormSubmit = event => {
+  event.preventDefault();
+
+  feedbackFormEl.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+feedbackFormEl.addEventListener('input', throttle(onFeedbackFormFieldChange, 500));
+feedbackFormEl.addEventListener('submit', onFeedbackFormSubmit);
